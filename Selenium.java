7161c3f839d46d5b565d18c84d6f2339bfae7a0d
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2026-08-18 09:44:53 +0530

    Update

---
+++ Selenium.java
@@ -0,0 +1,42 @@
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class SeleniumGridDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        String browser = "chrome"; // Change to "firefox" if required
+
+        WebDriver driver;
+
+        if (browser.equalsIgnoreCase("chrome")) {
+
+            ChromeOptions options = new ChromeOptions();
+
+            driver = new RemoteWebDriver(
+                    new URL("http://localhost:4444"),
+                    options
+            );
+
+        } else {
+
+            FirefoxOptions options = new FirefoxOptions();
+
+            driver = new RemoteWebDriver(
+                    new URL("http://localhost:4444"),
+                    options
+            );
+        }
+
+        driver.get("https://www.google.com");
+
+        System.out.println("Browser: " + browser);
+        System.out.println("Page Title: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
