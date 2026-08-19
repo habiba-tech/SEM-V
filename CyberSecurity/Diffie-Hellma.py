1e1a056f5bc52fbb4c54974a286b40eebb763d87
habiba Siddiqui <siddiquiummehabiba41@gmail.com>
2026-08-19 08:56:18 +0530

    Implementing the Diffie-Hellam

---
+++ CyberSecurity/Diffie-Hellma.py
@@ -0,0 +1,26 @@
+# Diffie-Hellman Key Exchange 
+
+p = int(input("Enter prime number (p): "))
+g = int(input("Enter primitive root (g)"))
+
+a = int(input("Enter Alice's private key : "))
+b = int(input("Enter Bob's private key : "))
+
+#public keys
+A = pow(g,a,p)
+B = pow(g,b,p)
+
+#Shared Secret Keys
+KA = pow(B, a, p)
+KB = pow(A, b, p)
+
+print("\nAlice's Public key:", A)
+print("BOb's public key:",B)
+
+print("\nAlice's Public key:", KA)
+print("BOb's public key:",KB)
+
+if KA == KB :
+    print("\nKey Exchange Sucessful !")
+else:
+    print("\n key Exchange Failed !")
